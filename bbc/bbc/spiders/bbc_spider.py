7942ae818cf55a23d951aafc7ba5c38afde3069c
jezeelmohd <jezeelmohd@gmail.com>
2014-02-20 20:13:42 +0530

    Rule added properly

--- bbc/bbc/spiders/bbc_spider.py
+++ bbc/bbc/spiders/bbc_spider.py
@@ -1,12 +1,10 @@
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from getevents.consistency_check import consistency_check
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.log import ScrapyFileLogObserver
 from scrapy.shell import inspect_response
 from scrapy.spider import BaseSpider
 from scrapy.selector import Selector
 from scrapy.http import FormRequest
-from getevents.items import *
 from scrapy.http import Request
 from dateutil import parser
 from time import sleep
@@ -21,5 +19,8 @@ class BBCspider(CrawlSpider):
 	allowed_domains = ['bbc.co.uk']
 
 	rules = (
-		Rule(SgmlLinkExtractor(restrict_xpaths=('//div[@class="mb10"]//a','//div[@class="span8"]/div/ul[1]//a')),follow=True,callback='parse_event'),
+		Rule(SgmlLinkExtractor(allow=('bbc.co.uk\/news\/.+')),follow=True,callback='parse_news'),
 		)
+
+	def parse_news(self,response):
+		print '>>>>>>>>',response.url
